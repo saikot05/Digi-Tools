5241d9b3af1ea89afafe2d22517d9b85ec62798f
saikot05 <saikotislam08@gmail.com>
2026-03-30 17:47:05 +0600

    design navbar according to figma

--- src/componets/navbar/Navbar.jsx
+++ src/componets/navbar/Navbar.jsx
@@ -4,23 +4,35 @@ import { MdOutlineShoppingCart } from 'react-icons/md';
 const Navbar = () => {
     return (
         <div className="navbar bg-base-100 shadow-sm">
-            <div className="navbar-start">
-                <a className="btn btn-ghost text-xl">DigiTools</a>
+            <div className="max-w-6xl mx-auto w-full flex items-center justify-between">
+                <div className="navbar-start">
+                <div className="dropdown">
+                    <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
+                    </div>
+                    <ul tabIndex={0} className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
+                        <li><a>Products</a></li>
+                        <li><a>Features</a> </li>
+                        <li><a>Pricing</a></li>
+                        <li><a>Testimonials</a></li>
+                        <li><a>FAQ</a></li>
+                    </ul>
+                </div>
+                <h2 className="btn btn-ghost text-2xl bg-clip-text text-transparent bg-gradient-to-r from-[#4F39F6] to-purple-500">DigiTools</h2>
             </div>
-            <div className="navbar-center">
+            <div className="navbar-center hidden lg:flex">
                 <ul className="menu menu-horizontal px-1">
                 <li><a>Products</a></li>
-                <li>
-                    <a>Features</a> 
-                </li>
+                <li><a>Features</a> </li>
                 <li><a>Pricing</a></li>
                 <li><a>Testimonials</a></li>
                 <li><a>FAQ</a></li>
                 </ul>
             </div>
-            <div className="navbar-end gap-2">
-                <p className='flex items-center gap-2'><MdOutlineShoppingCart />Login</p>
-                <a className="btn btn-primary rounded-full">Get Started</a>
+            <div className="navbar-end gap-4">
+                <button className='flex items-center gap-2'><MdOutlineShoppingCart />Login</button>
+                <a className="btn btn-primary rounded-full bg-gradient-to-r from-[#4F39F6] to-purple-500 border-none">Get Started</a>
+            </div>
             </div>
         </div>
     );
